a4df7f51104efe059bc46d5ae2e537f60a449be4
laziz <qudratov.com26@gmail.com>
2026-06-12 16:19:58 +0500

    changed response

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ app.get("/app/qudratov_com26_gmail_com", (req, res) => {
   const rawX = req.query.x;
   const rawY = req.query.y;
 
-  if (!Number.isInteger(Number(rawX)) || !Number.isInteger(Number(rawY)) || !rawX || !rawY || Number(rawX) === 0 || Number(rawY) === 0){
-    return res.type("text/plain").send("NaN");
+  if (!Number.isInteger(Number(rawX)) || !Number.isInteger(Number(rawY)) || !rawX || !rawY || Number(rawX) <= 0 || Number(rawY) <= 0){
+    return res.text("NaN");
   }
 
   const x = Number(rawX);
   const y = Number(rawY);
 
-  res.type("text/plain").send(lcm(x, y));
+  res.text(lcm(x, y));
 });
 
 function lcm(x, y) {
   const bx = BigInt(x);
   const by = BigInt(y);
-  return String(bx * by / gcd(bx, by));
+  return Number(bx * by / gcd(bx, by));
 }
 
 function gcd(x, y) {
